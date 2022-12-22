f74cce98156e49fa3c5274e51134379979c0aac0
Jason <jharvey0232@gmail.com>
2022-12-22 07:39:46 -0500

    section 15 complete

---
+++ js/sect15.js
@@ -0,0 +1,9 @@
+const dayOfWeek = 'Monday';
+if (dayOfWeek === "Monday") {
+    console.log("It is Monday.")
+
+} else if (dayOfWeek === "Saturday") {
+    console.log("I love Saturday.")
+}
+
+console.log(dayOfWeek);
