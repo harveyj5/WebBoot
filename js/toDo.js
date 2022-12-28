46dd01297d1821f70a85a283d705cf7fdc431e68
Jason <jharvey0231@gmail.com>
2022-12-28 05:48:45 -0500

    starting new section

--- js/toDo.js
+++ js/toDo.js
@@ -1,30 +1,29 @@
 //prompt on actions Add, List, Delete, quit
 let input= prompt("Please enter a command add, list, delete or 'q': ");
+const toDoList = ['Collet Eggs', 'Feed Chickens'];
+
+
 //Loop until q is entered
 while (input !== 'quit' && input !== 'q'){
 
 
-const toDoList = ['Collet Eggs', 'Feed Chickens'];
-
-
-//Create an add to list using push
-    // if (input === 'add'){
-    //     toDoList.push = prompt("Please add a todo item: ");
-    //     console.log(toDoList);
-    //     input= prompt("Please enter a command add, list, delete or 'q': ");
-    // }
 // list all items in the to do for of using literal
  if (input === 'list'){
     for(let i =0; i < toDoList.length; i++){
     console.log(`${i}: ${toDoList[i]}`);
     }
 
+ }else if (input === 'add'){
+    const addToDo = prompt('What is the new todo: ');
+    toDoList.push(addToDo);
+    console.log(`New Todo ${addToDo} has been added to the list`);
+ }else if (input === 'delete'){
+    const indexPrompt = prompt('What task number would you like to delete: ');
+    deleted = toDoList.splice(indexPrompt,1);
+    console.log(`The following ${deleted} has been deleted`);
+
  }
-// delete list by index
-    // if(input === "delete"){
-    // let listItem = parseInt(prompt("What list item would you like to delete: "));
-    // toDoList.remove[listItem];
-    // console.log(toDoList);
+
  input= prompt("Please enter a command add, list, delete or 'q': ");
     // }
     
