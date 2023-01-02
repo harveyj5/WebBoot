f2161ffbd027ee7ae5ec346d89a83f566e1deaac
Jason <jharvey0231@gmail.com>
2023-01-02 08:46:20 -0500

    Moving to next section

---
+++ js/sect20.js
@@ -0,0 +1,9 @@
+// DEFINE YOUR FUNCTION BELOW:
+function capitalize(str){
+    let newStr = str
+    newStr.charAt(0).capitalize;
+    return newStr;
+  }
+  capitalize('computer'); // "Eggplant"
+  capitalize('pamplemousse'); // "Pamplemousse"
+  capitalize('squid'); //"Squid"
